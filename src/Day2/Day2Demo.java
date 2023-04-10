7318416b8d6e76e6b7cc43102df4bfbca80870b1
18652 <18652@10.0.0.145>
2023-04-10 11:56:06 -0400

    added more

--- src/Day2/Day2Demo.java
+++ src/Day2/Day2Demo.java
@@ -1,13 +1,9 @@
 package Day2;
 
 public class Day2Demo {
-	public static void main(String[] args) {
+	public  void getinfo() {
 		
-		System.out.println("hi");
+	
+		System.out.println("push");
 	}
-	public void newAdd( int a) {
-		
-		System.out.println(" Hello");
-	}
-
 }
